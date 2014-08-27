0778b3eca62cb6eb5c1c0f6b4e28b041d47c184e
Joshua <moy@MacBook-Pro-de-Moises.local>
2014-08-26 20:54:40 -0500

    Initial Commit

---
+++ sqlite3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sqlite3
+//
+//  Created by Joshua on 26/08/14.
+//  Copyright (c) 2014 Joshua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ sqlite3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  sqlite3
+//
+//  Created by Joshua on 26/08/14.
+//  Copyright (c) 2014 Joshua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
